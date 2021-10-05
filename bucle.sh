94208c6307ff0703466534ad615ecdf97fec448f
David <190290@iessierradeguara.com>
2021-10-05 11:55:59 +0200

    git@github.com:dcp37/PSP-test2.git

--- bucle.sh
+++ bucle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #hola que tal put0 el que lo lea
 rep=1000000
-for ((int i = 0; i <$rep; i++)){
-    do
-        echo "hola $i" >> salida.txt
-    done
-}
-'
+for ((i=0; i <=$rep; i++))
+do
+    echo "hola $i" >> salida.txt
+done
+
+:'
 for i in $(seq 1 $rep)
 do 
     echo "hola $i" >> salida.txt
---
+++ retorno.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#prgrama que devuelve el doble del primer parametro
+echo $1*2
